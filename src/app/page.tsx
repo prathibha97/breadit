b4bbde728cf9743e07e2f3cdae2c2a9de7091683
Prathibha Ratnayake <prsthibha@gmail.com>
2023-07-11 00:42:26 +0530

    Create custom and general feed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
+import CustomFeed from '@/components/CustomFeed';
+import GeneralFeed from '@/components/GeneralFeed';
 import { buttonVariants } from '@/components/ui/Button';
+import { getAuthSession } from '@/lib/auth';
 import { HomeIcon } from 'lucide-react';
 import Link from 'next/link';
 
-export default function Home() {
+export default async function Home() {
+  const session = await getAuthSession();
   return (
     <>
       <h1>Your Feed</h1>
       <div className='grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6'>
-        {/* Feed */}
+        {/* @ts-expect-error server component */}
+        {session ? <CustomFeed /> : <GeneralFeed />}
 
         {/* Subreddit Info */}
         <div className='overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last'>
@@ -20,14 +25,18 @@ export default function Home() {
           <div className='-my-3 divide-y divide-gray-100 px-6 py-4 text-sm leading-6'>
             <div className='flex justify-between gap-x-4 py-3'>
               <p className='text-zinc-500'>
-                Your personal Breaddit homepage. Come here to check in with your favourite communities.
+                Your personal Breaddit homepage. Come here to check in with your
+                favourite communities.
               </p>
             </div>
-            <Link className={buttonVariants({
-              className: 'w-full mt-4 mb-6'
-            })}
-            href='/r/create'
-            >Create Community</Link>
+            <Link
+              className={buttonVariants({
+                className: 'w-full mt-4 mb-6',
+              })}
+              href='/r/create'
+            >
+              Create Community
+            </Link>
           </div>
         </div>
       </div>
