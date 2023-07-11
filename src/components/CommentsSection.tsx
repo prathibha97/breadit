ff0db8846ccdf70feaa5f69f90296029df3a7601
Prathibha Ratnayake <prsthibha@gmail.com>
2023-07-11 11:30:08 +0530

    Add reply to comment functionality

--- src/components/CommentsSection.tsx
+++ src/components/CommentsSection.tsx
@@ -1,57 +1,102 @@
-import { getAuthSession } from "@/lib/auth";
-import { db } from "@/lib/db";
-import PostComment from "./PostComment";
-import CreateComment from "./CreateComment";
+import { getAuthSession } from '@/lib/auth';
+import { db } from '@/lib/db';
+import CreateComment from './CreateComment';
+import PostComment from './PostComment';
 
 interface CommentsSectionProps {
   postId: string;
 }
 
-const CommentsSection = async({ postId }: CommentsSectionProps) => {
-
-  const session = await getAuthSession()
+const CommentsSection = async ({ postId }: CommentsSectionProps) => {
+  const session = await getAuthSession();
 
   const comments = await db.comment.findMany({
-    where:{
+    where: {
       postId,
-      replyToId: null
+      replyToId: null,
     },
-    include:{
+    include: {
       author: true,
       votes: true,
-      replies:{
-        include:{
+      replies: {
+        include: {
           author: true,
           votes: true,
-        }
-      }
-    }
-  })
+        },
+      },
+    },
+  });
 
-  return <div className="flex flex-col gap-y-4 mt-4">
-    <hr className="w-full h-px my-6"/>
+  return (
+    <div className='flex flex-col gap-y-4 mt-4'>
+      <hr className='w-full h-px my-6' />
 
-   <CreateComment postId={postId} />
+      <CreateComment postId={postId} />
 
-    <div className="flex flex-col gap-y-6 mt-4">
-      {comments.filter((comment) => !comment.replyToId).map((topLevelComment)=>{
+      <div className='flex flex-col gap-y-6 mt-4'>
+        {comments
+          .filter((comment) => !comment.replyToId)
+          .map((topLevelComment) => {
+            const topLevelCommentVotesAmt = topLevelComment.votes.reduce(
+              (acc, vote) => {
+                if (vote.type === 'UP') return acc + 1;
+                if (vote.type === 'DOWN') return acc + 1;
+                return acc;
+              },
+              0
+            );
 
-        const topLevelCommentVotesAmt = topLevelComment.votes.reduce((acc, vote)=>{
-          if(vote.type === 'UP') return acc + 1
-          if (vote.type === 'DOWN') return acc + 1;
-          return acc
-        },0)
+            const topLevelCommentVote = topLevelComment.votes.find(
+              (vote) => vote.userId === session?.user.id
+            );
 
-        const topLevelCommentVote = topLevelComment.votes.find((vote)=> vote.userId === session?.user.id)
+            return (
+              <div key={topLevelComment.id} className='flex flex-col'>
+                <div className='mb-2'>
+                  <PostComment
+                    comment={topLevelComment}
+                    currentVote={topLevelCommentVote}
+                    postId={postId}
+                    votesAmt={topLevelCommentVotesAmt}
+                  />
+                </div>
 
-        return <div key={topLevelComment.id} className="flex flex-col">
-          <div className="mb-2">
-            <PostComment comment={topLevelComment} />
-          </div>
-        </div>
-      })}
+                {/* Render replies */}
+                {topLevelComment.replies
+                  .sort((a, b) => b.votes.length - a.votes.length)
+                  .map((reply) => {
+                    const replyVotesAmt = topLevelComment.votes.reduce(
+                      (acc, vote) => {
+                        if (vote.type === 'UP') return acc + 1;
+                        if (vote.type === 'DOWN') return acc + 1;
+                        return acc;
+                      },
+                      0
+                    );
+
+                    const replyVote = topLevelComment.votes.find(
+                      (vote) => vote.userId === session?.user.id
+                    );
+                    return (
+                      <div
+                        key={reply.id}
+                        className='ml-2 py-2 pl-4 border-l-2 border-zinc-200'
+                      >
+                        <PostComment
+                          comment={reply}
+                          currentVote={replyVote}
+                          votesAmt={replyVotesAmt}
+                          postId={postId}
+                        />
+                      </div>
+                    );
+                  })}
+              </div>
+            );
+          })}
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default CommentsSection;
--- src/components/PostComment.tsx
+++ src/components/PostComment.tsx
@@ -1,8 +1,19 @@
 'use client';
-import { Comment, CommentVote, User } from '@prisma/client';
-import { FC, useRef } from 'react';
-import { UserAvatar } from './UserAvatar';
+import { toast } from '@/hooks/use-toast';
 import { formatTimeToNow } from '@/lib/utils';
+import { CommentRequest } from '@/lib/validators/comment';
+import { Comment, CommentVote, User } from '@prisma/client';
+import { useMutation } from '@tanstack/react-query';
+import axios from 'axios';
+import { MessageSquare } from 'lucide-react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { FC, useRef, useState } from 'react';
+import CommentVotes from './CommentVotes';
+import { UserAvatar } from './UserAvatar';
+import { Button } from './ui/Button';
+import { Label } from './ui/Label';
+import { Textarea } from './ui/Textarea';
 
 type ExtendedComment = Comment & {
   votes: CommentVote[];
@@ -11,10 +22,53 @@ type ExtendedComment = Comment & {
 
 interface PostCommentProps {
   comment: ExtendedComment;
+  currentVote: CommentVote | undefined;
+  votesAmt: number;
+  postId: string;
 }
 
-const PostComment: FC<PostCommentProps> = ({ comment }) => {
+const PostComment: FC<PostCommentProps> = ({
+  comment,
+  currentVote,
+  votesAmt,
+  postId,
+}) => {
   const commentRef = useRef<HTMLDivElement>(null);
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  const [isReplying, setIsReplying] = useState<boolean>(false);
+  const [input, setInput] = useState<string>('');
+
+  const { mutate: postComment, isLoading } = useMutation({
+    mutationFn: async ({ postId, text, replyToId }: CommentRequest) => {
+      const payload: CommentRequest = {
+        postId,
+        text,
+        replyToId,
+      };
+
+      const { data } = await axios.patch(
+        `/api/subreddit/post/comment`,
+        payload
+      );
+      return data;
+    },
+    onError: () => {
+      return toast({
+        title: 'Something went wrong',
+        description:
+          'Comment was not posted successfully, please try again later',
+        varient: 'destructive',
+      });
+    },
+    onSuccess: () => {
+      router.refresh();
+      setIsReplying(false);
+      setInput('');
+    },
+  });
+
   return (
     <div ref={commentRef} className='flex flex-col'>
       <div className='flex items-center'>
@@ -37,6 +91,65 @@ const PostComment: FC<PostCommentProps> = ({ comment }) => {
       </div>
 
       <p className='text-sm text-zinc-900 mt-2'>{comment.text}</p>
+
+      <div className='flex flex-wrap gap-2 items-center '>
+        <CommentVotes
+          commentId={comment.id}
+          initialVotesAmt={votesAmt}
+          initialVote={currentVote}
+        />
+
+        <Button
+          onClick={() => {
+            if (!session) return router.push('/sign-in');
+            setIsReplying(true);
+          }}
+          variant='ghost'
+          size='xs'
+        >
+          <MessageSquare className='h-4 w-4 mr-1.5' />
+          Reply
+        </Button>
+
+        {isReplying ? (
+          <div className='grid w-full gap-1.5'>
+            <Label htmlFor='comment'>Your Comment</Label>
+            <div className='mt-2'>
+              <Textarea
+                id='comment'
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                rows={1}
+                placeholder='What are your thoughts?'
+              />
+
+              <div className='mt-2 flex justify-end'>
+                <Button
+                  tabIndex={-1}
+                  variant='subtle'
+                  onClick={() => setIsReplying(false)}
+                >
+                  Cancel
+                </Button>
+                <Button
+                  isLoading={isLoading}
+                  disabled={input.length === 0}
+                  onClick={() => {
+                    if (!input) return;
+                    postComment({
+                      postId,
+                      text: input,
+                      replyToId: comment.replyToId ?? comment.id,
+                    });
+                  }}
+                >
+                  Post
+                </Button>
+              </div>
+            </div>
+          </div>
+        ) : null}
+      </div>
     </div>
   );
 };
